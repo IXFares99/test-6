447f8eafa22f4ccbce219e7ed58bca1f870acf90
IXFares99 <52209189+IXFares99@users.noreply.github.com>
2019-06-28 21:36:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("589421447974486047")
 setInterval(function() {
 channel.send(`iyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi iyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalbiyouchi kalb`);
-}, 30)
+}, 10)
 })
 
 client.login(process.env.BOT_TOKEN);
