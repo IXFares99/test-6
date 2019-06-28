ac1d835aed26a44b46cfa1f1bef3535811f0f859
IXFares99 <52209189+IXFares99@users.noreply.github.com>
2019-06-28 22:06:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("")
+let channel =     client.channels.get("589421447974486047")
 setInterval(function() {
-channel.send(``);
-}, 30)
+channel.send(`lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay  lay lay lay lay laylay laylaylaylaylay lay lay lay laylay laylay lay lay lay`);
+}, 10)
 })
 
 client.login(process.env.BOT_TOKEN);
